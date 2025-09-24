83d0942c586cccd47c40008d78e9677876712db5
danielshernandez88 <daniel.shernandez88@gmail.com>
2025-09-23 23:01:00 -0400

    Action2 completed

--- custOrders_main.py
+++ custOrders_main.py
@@ -14,6 +14,26 @@ customer_purchases = { 'Jose': ['Laptop', 'Smartphone', 'Jacket'],
                        'Luis': ['Blender', 'Coffee Maker', 'Smartwatch']}
 
 
-print("Lista de Clientes y sus compras:")
-for details in customer_details:
-    print(details)
+"""" . Classify products by category 
+• Use a dictionary to map each product to its respective category 
+• Create a set of unique product categories 
+• Display all available product categories """
+
+customer_dict = {'Electronics': set(), 'Clothing': set(), 'Home Essentials': set()}
+
+category_electronics = {'Laptop', 'Smartphone', 'Smartwatch'}
+category_clothing = {'Dress', 'Jacket', 'Shoes', 'T-shirt'}
+category_home_essentials = {'Coffee Maker', 'Blender', 'Vacuum Cleaner', 'Microwave'}
+
+for category in customer_dict.keys():
+    for customer, products in customer_purchases.items():
+        for product in products:
+            if product in category_electronics and category == 'Electronics':
+                customer_dict[category].add(product)
+            elif product in category_clothing and category == 'Clothing':
+                customer_dict[category].add(product)
+            elif product in category_home_essentials and category == 'Home Essentials':
+                customer_dict[category].add(product)   
+
+
+print(customer_dict)
